543bd5750810833efcc35d0afbf3a131fcfbb155
ShimaaElnaggar <shimaaibrahim072@gmail.com>
2024-08-14 02:11:38 +0300

    create models

---
+++ lib/models/certificate.dart
@@ -0,0 +1,16 @@
+class Certificate {
+  final String name;
+  final String organization;
+  final String date;
+  final String skills;
+  final String credential;
+
+  Certificate({
+    required this.name,
+    required this.organization,
+    required this.date,
+    required this.skills,
+    required this.credential,
+  });
+}
+
